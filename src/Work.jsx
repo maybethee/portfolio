34235a89c9c45f4fecc13b419073d9f62771b635
retra <jozalamun@gmail.com>
2026-09-05 12:29:16 -0400

    polish table styles and fix up other little things

--- src/Work.jsx
+++ src/Work.jsx
@@ -4,16 +4,86 @@ function Work() {
   return (
     <>
       <div id={styles.wrapper}>
-        <table>
+        <table id={styles.workTable}>
           <thead>
             <tr>
-              <th>Title</th>
+              <th>Work</th>
               <th>Publisher/Employer</th>
               <th>Year</th>
-              <th>Type</th>
+              <th>Work Type</th>
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td>
+                <a href="https://uglyducklingpresse.org/publications/the-glass-clouding/">
+                  The Glass Clouding by Masaoka Shiki
+                </a>
+              </td>
+              <td>Ugly Duckling Presse</td>
+              <td>2024</td>
+              <td>Editing/Typesetting</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://medium.com/ugly-duckling-presse/backlist-bulletin-10-lowly-by-alan-felsenthal-907e0854d394">
+                  Backlist Bulletin #10: Lowly by Alan Felsenthal
+                </a>
+              </td>
+              <td>Ugly Duckling Presse</td>
+              <td>2021</td>
+              <td>Review/Essay</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://uglyducklingpresse.org/publications/air-raid/">
+                  Air Raid by Polina Barskova
+                </a>
+              </td>
+              <td>Ugly Duckling Presse</td>
+              <td>2021</td>
+              <td>Typesetting</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://uglyducklingpresse.org/product/what-is-abomination/">
+                  What Is Abomination by Tomaž Šalamun
+                </a>
+              </td>
+              <td>Ugly Duckling Presse</td>
+              <td>2021</td>
+              <td>Broadside Design/Letterpress Printing</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://uglyducklingpresse.org/publications/morning-poem-undie-popovers/">
+                  Morning Poem (Undie Popovers) by Farnoosh Fathi
+                </a>
+              </td>
+              <td>Ugly Duckling Presse</td>
+              <td>2021</td>
+              <td>Broadside Design/Letterpress Printing</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://www.poetryproject.org/publications/newsletter/265-summer-2021/lawrence-giffin-interviews-jean-day-on-late-human-udp-2021?page=1">
+                  Lawrence Giffin Interviews Jean Day on Late Human (UDP, 2021)
+                </a>
+              </td>
+              <td>The Poetry Project Newsletter</td>
+              <td>2020</td>
+              <td>Transcription</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://uglyducklingpresse.org/publications/spring-cleaning/">
+                  Spring Cleaning by Marshall Bood
+                </a>
+              </td>
+              <td>Ugly Duckling Presse</td>
+              <td>2020</td>
+              <td>Cover Design</td>
+            </tr>
             <tr>
               <td>
                 <a href="https://dreginald.com/index.php/issues/issue-fourteen/raphael-schnee">
@@ -73,76 +143,6 @@ function Work() {
               <td>Alien Mouth </td>
               <td>2015</td>
               <td>Poetry</td>
-            </tr>
-            <tr>
-              <td>
-                <a href="https://medium.com/ugly-duckling-presse/backlist-bulletin-10-lowly-by-alan-felsenthal-907e0854d394">
-                  Backlist Bulletin #10: Lowly by Alan Felsenthal
-                </a>
-              </td>
-              <td>Ugly Duckling Presse</td>
-              <td>2021</td>
-              <td>Review/Essay</td>
-            </tr>
-            <tr>
-              <td>
-                <a href="https://uglyducklingpresse.org/publications/spring-cleaning/">
-                  Spring Cleaning by Marshall Bood
-                </a>
-              </td>
-              <td>Ugly Duckling Presse</td>
-              <td>2021</td>
-              <td>Cover Design</td>
-            </tr>
-            <tr>
-              <td>
-                <a href="https://uglyducklingpresse.org/publications/air-raid/">
-                  Air Raid by Polina Barskova
-                </a>
-              </td>
-              <td>Ugly Duckling Presse</td>
-              <td>2021</td>
-              <td>Typesetting</td>
-            </tr>
-            <tr>
-              <td>
-                <a href="https://uglyducklingpresse.org/publications/the-glass-clouding/">
-                  The Glass Clouding by Masaoka Shiki
-                </a>
-              </td>
-              <td>Ugly Duckling Presse</td>
-              <td>2024</td>
-              <td>Typesetting</td>
-            </tr>
-            <tr>
-              <td>
-                <a href="https://uglyducklingpresse.org/publications/morning-poem-undie-popovers/">
-                  Morning Poem (Undie Popovers) by Farnoosh Fathi
-                </a>
-              </td>
-              <td>Ugly Duckling Presse</td>
-              <td>2021</td>
-              <td>Broadside Design/Printing</td>
-            </tr>
-            <tr>
-              <td>
-                <a href="https://uglyducklingpresse.org/product/what-is-abomination/">
-                  What is Abomination by Tomaž Šalamun
-                </a>
-              </td>
-              <td>Ugly Duckling Presse</td>
-              <td>2021</td>
-              <td>Broadside Design/Printing</td>
-            </tr>
-            <tr>
-              <td>
-                <a href="https://www.poetryproject.org/publications/newsletter/265-summer-2021/lawrence-giffin-interviews-jean-day-on-late-human-udp-2021?page=1">
-                  Lawrence Giffin Interviews Jean Day on Late Human (UDP, 2021)
-                </a>
-              </td>
-              <td>The Poetry Project Newsletter</td>
-              <td>2020</td>
-              <td>Transcription</td>
             </tr>
           </tbody>
         </table>
