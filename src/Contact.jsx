28c18a57b1e1621c162dff5b0e362e8509c150f1
retra <jozalamun@gmail.com>
2026-09-05 12:48:27 -0400

    fix incorrect color on dev site

---
+++ src/Contact.jsx
@@ -0,0 +1,17 @@
+import styles from "./Contact.module.css";
+
+function Contact() {
+  return (
+    <>
+      <div>
+        <p>
+          Feel free to email me at raphaelschnee.send@gmail.com for anything
+          related to writing, coding, or just to send me a photo of your
+          favorite lost pet photo.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
